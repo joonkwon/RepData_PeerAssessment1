f7d95a15f2a31f99fdb9b28b21e3de3ef61bb652
Joon Kwon <joon.kwon@bellmedia.ca>
2016-03-01 16:16:48 -0500

    daily sum completed

--- main.R
+++ main.R
@@ -20,7 +20,16 @@ mean.by.interval$interval <- as.numeric(as.character(mean.by.interval$interval))
 mean.by.interval.sorted <- mean.by.interval[order(mean.by.interval$interval),]
 with(mean.by.interval.sorted, plot(interval, mean.steps, type="l"))
 
+data.missing <- data[is.na(data$steps),]
+dim(data.missing)
 
-
+data.filled <- merge(data, mean.by.interval.sorted, by.x = "interval", by.y="interval")
+data.filled <- transform(data.filled, steps = ifelse(is.na(steps), mean.steps, steps))
+data.filled <- select(data.filled, 1:3)
+#data.filled$interval <- as.numeric(as.character(data.filled$interval))
+#data.filled <- data.filled[order(data.filled$interval),]
+data.filled.by.date <- group_by(data.filled, date)
+sum.by.date <- summarise(data.filled.by.date, daily.sum=sum(steps))
+hist(sum.by.date$daily.sum)
 
 
